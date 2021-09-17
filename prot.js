318d4e696c942581706ee5355cc5376978d072ad
Omer Kirshner <90328782+OmerKH@users.noreply.github.com>
2021-09-17 10:24:21 +0300

    Add files via upload
    Updated site

--- prot.js
+++ prot.js
@@ -13,3 +13,18 @@ hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
 min.style.transform = `rotateZ(${mm}deg)`;
 sec.style.transform = `rotateZ(${ss}deg)`;
  });
+
+ /------------------/
+ const next = document.querySelector(".cardousel__btn--next"),
+ back = document.querySelector(".cardousel__btn--back"),
+ carousel = document.querySelector(".picards");
+let angle = 0;
+
+next.addEventListener("click", () => {
+    angle -= 45;
+    carousel.style.transform = `translateZ(-25rem) rotateY(${angle}deg)`;
+  });
+back.addEventListener("click", () => {
+    angle += 45;
+    carousel.style.transform = `translateZ(-25rem) rotateY(${angle}deg)`;
+  });
